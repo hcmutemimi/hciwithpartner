9ac8b89c648d08366231dd02ca2bae20b7defd57
hcmutemimi <hoaimiqng@gmail.com>
2020-06-05 16:14:19 +0700

    2 commit

--- Content/carousel/carousel.js
+++ Content/carousel/carousel.js
@@ -1,7 +1,58 @@
-$('.owl-carousel').owlCarousel({
-    items: 4,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 1500,
-    autoplayHoverPause: true
+$('.carousel-main').owlCarousel({
+  
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayHoverPause:true,
+responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 2000,
+  smartSpeed: 800,
+  //nav: true,
+      responsive: {
+      0: {
+        items: 1
+      },
+
+      600: {
+        items: 3
+      },
+
+      1024: {
+        items: 6
+      }
+    }
+//nav: true,
+// dots: false,
+// navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
+})
+$('.carousel-second').owlCarousel({
+  loop:true,
+  margin:10,
+  autoplayHoverPause:true,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 5000,
+  smartSpeed: 3000,
+  //nav: true,
+  responsive: {
+  0: {
+    items: 1
+  },
+
+  600: {
+    items: 2
+  },
+
+  1024: {
+    items: 4
+  },
+
+  1349: {
+    items: 4
+  }
+}
+//nav: true,
+// dots: false,
+// navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
 })
