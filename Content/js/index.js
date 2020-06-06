1ef6715af47f1211d3dd0b9569a05d6219d64c6c
hcmutemimi <hoaimiqng@gmail.com>
2020-06-06 17:39:26 +0700

    6 commit

--- Content/js/index.js
+++ Content/js/index.js
@@ -1,10 +1,23 @@
-$('.carousel-main').owlCarousel({
-	items: 3,
-	loop: true,
-	autoplay: true,
-	autoplayTimeout: 1500,
-	margin: 10,
-	// nav: true,
-	// dots: false,
-	// navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
-})
+document.addEventListener("DOMContentLoaded",function(){
+    var menunho = document.querySelector('.row-nav');
+    
+    window.addEventListener('scroll',function(){
+        if(window.pageYOffset>45){
+            menunho.classList.add('navbar-larger')
+        }
+        else{
+            menunho.classList.remove('navbar-larger')
+		}
+	});
+	
+	var blocksearch = document.querySelector('.header-search-block');
+    window.addEventListener('scroll',function(){
+        if(window.pageYOffset >142){
+            blocksearch.classList.add('header-search-block-small')
+        }else{
+            blocksearch.classList.remove('header-search-block-small')
+
+        }
+    });
+   
+},false)
